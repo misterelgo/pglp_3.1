54eca9e574ad14fd6e974e66e499668637602488
misterelgo <misterelgo@gmail.com>
2020-02-26 17:12:35 +0100

    Implémentation de la classe Employe

--- src/main/java/Employe.java
+++ src/main/java/Employe.java
@@ -1,2 +1,12 @@
-public class Employe {
+public class Employe extends AllEmploye{
+    final private int salaire = 1500;
+    private int startYear;
+    public Employe(int startYear){
+        this.startYear = startYear;
+    }
+
+    public double salaire() {
+        return salaire+ (2020-this.startYear)*20;
+
+    }
 }

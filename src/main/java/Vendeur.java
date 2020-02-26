78e7ea4e1f55c6de5515d4df8841b6ab621d3679
misterelgo <misterelgo@gmail.com>
2020-02-26 17:12:58 +0100

    Implémentation de la classe Vendeur*

--- src/main/java/Vendeur.java
+++ src/main/java/Vendeur.java
@@ -1,2 +1,11 @@
-public class Vendeur {
+public class Vendeur extends AllEmploye{
+    final private int base = 2000;
+    private int commission;
+    public Vendeur(int commission){
+        this.commission = commission;
+    }
+
+    public double salaire() {
+        return base+commission;
+    }
 }
